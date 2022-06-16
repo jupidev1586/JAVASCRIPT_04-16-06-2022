c1be694297d55e12b0a38a6cf71498a0dae06565
jupidev1586 <gianpaolobuono@gmail.com>
2022-06-16 19:50:00 +0200

    feat: calculator

--- script.js
+++ script.js
@@ -1,58 +1,99 @@
-// console.log('Ciao');
+// document.onkeydown = checkKeycode
 
-// alert('Ciao');
-
-// let usrMsg = prompt('Inserisci qualcosa');
-
-
-// Calculator - PSEUDO
-// 1. Chiede 2 numeri da input all'utente
-//  -- IF se l'utente non inserisce 1 o entrambi i numeri, ritorna una stringa che ci informa su questo
-//     e interrompe il ciclo del programma
-
-// 2. Chiede all'utente l'operazione da eseguire
-// -- IF se l'utente non inserisce alcuna operazione, ritorna una stringa che ci informa su questo
-
-// 3. Venga mostrato il risultato dell'operazione
-
-
-// prompt restituisce una string, bisogna convertire in numero
-
-// Calculator - CODE
-const firstNumber = prompt('Inserisci il primo numero');
-const secondNumber = prompt('Inserisci il secondo numero');
-
-const operationChoice = prompt('Inserisci pure l\'operazione desiderata')
-
-const parsedFirstNum = parseInt(firstNumber);
-const parsedSecondNum = parseInt(secondNumber);
-
-
-if (firstNumber.length == 0 || secondNumber.length == 0) {
-  // 1. Chiede 2 numeri da input all'utente
-  //  -- IF se l'utente non inserisce 1 o entrambi i numeri, ritorna una stringa che ci informa su questo
-  //     e interrompe il ciclo del programma
-  console.log('Non hai inserito un numero');
-} 
-
-// if (operationChoice === 'addizione' && secondNumber.length == 0) {
-//   // 
-//   console.log('il risultato della tua operazione è:', parsedFirstNum + parsedSecondNum)
+// function checkKeycode(e) {
+//   let keycode;
+//   if (window.event)
+//     {keycode = window.event.keyCode;}
+//   else if (e)
+//     {keycode = e.which;}
+//   alert("keycode: " + keycode);
 // }
 
-switch(operationChoice) {
-  case '+' || 'addizione':
-    console.log('il risultato della tua operazione è:', parsedFirstNum + parsedSecondNum);
-    break;
-  case '-' || 'sottrazione':
-    console.log('il risultato della tua operazione è:', parsedFirstNum - parsedSecondNum);
-    break;
-  case '*' || 'moltiplicazione':
-    console.log('il risultato della tua operazione è:', parsedFirstNum * parsedSecondNum);
-    break;
-  case '/' || 'divisione':
-    console.log('il risultato della tua operazione è:', parsedFirstNum / parsedSecondNum);
-    break;
-  default:
-    console.log('Non hai selezionato alcuna operazione');
+
+// This function clear all the values
+function clearScreen() {
+  document.getElementById("answer").value = "";
 }
+
+
+// This function display values
+function display(value) {
+  document.getElementById("answer").value += value;
+}
+
+// This function evaluates the expression and return result
+function calculate() {
+  let p = document.getElementById("answer").value;
+  let q = eval(p);
+  document.getElementById("answer").value = q;
+}
+
+
+// const keydowned = document.getElementsByTagName('input');
+
+// document.addEventListener('click' , function () {
+//   for (let i = 0; i < keydowned.length; i++) {
+//     keydowned[i].classList.toggle('hovered-key');
+//   } 
+// });
+
+
+
+document.addEventListener('keydown', function(event) {
+  switch(event.keyCode) {
+    case  13:
+      calculate();
+      break;
+    case  46:
+      clearScreen();
+      break;
+    case  96:
+      display(0);
+      break;
+    case  97:
+      display(1);
+      break;
+    case  98:
+      display(2);
+      break;
+    case  99:
+      display(3);
+      break;
+    case  100:
+      display(4);
+      break;
+    case  101:
+      display(5);
+      break;
+    case  102:
+      display(6);
+      break;
+    case  103:
+      display(7);
+      break;
+    case  104:
+      display(8);
+      break;
+    case  105:
+      display(9);
+      break;
+    case  107:
+      display('+');
+      break;
+    case  109:
+      display('-');
+      break;
+    case  106:
+      display('*');
+      break;
+    case  111:
+      display('*');
+      break;
+    default:
+      console.log('Non hai selezionato alcuna operazione');
+  }
+
+  
+
+
+});
